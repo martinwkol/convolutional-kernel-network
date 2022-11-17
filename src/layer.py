e37a1edbb7f44370d3d891ff5d578d9bfc8f1434
Martin Koloseus <mkoloseus@posteo.de>
2022-11-17 18:13:07 +0100

    refactor(layer): use np.pad function for zero padding

--- src/layer.py
+++ src/layer.py
@@ -35,48 +35,38 @@ class layer:
         return self._filter
 
     def extract_patches(self, input):
-        (channels, pixels) = input.shape
-
         x_patch_radius = (self._patch_size[0] - 1) // 2
         y_patch_radius = (self._patch_size[1] - 1) // 2
-
-        x_diff_range = range( -x_patch_radius, x_patch_radius + 1 )
-        y_diff_range = range( -y_patch_radius, y_patch_radius + 1 )
-
-        x_offset = 0 if self._use_zero_padding else x_patch_radius
-        y_offset = 0 if self._use_zero_padding else y_patch_radius
         
-        patch_mx_size = (self._input_size[0] - x_offset * 2, self._input_size[1] - y_offset * 2)
-        patch_mx = np.zeros((channels * self._patch_size[0] * self._patch_size[1], patch_mx_size[0] * patch_mx_size[1]))
+        input = np.reshape(input, (self._num_channels, self._input_size[0], self._input_size[1]))
 
-        current_patch_mx_row = 0
-        for y_diff in y_diff_range:
-            for x_diff in x_diff_range:
-                if self._use_zero_padding:
-                    patch_mx_x_range = range(max(0, -x_diff), patch_mx_size[0] + min(0, -x_diff))
-                    patch_mx_y_range = range(max(0, -y_diff), patch_mx_size[1] + min(0, -y_diff))
-                else:
-                    patch_mx_x_range = range(patch_mx_size[0])
-                    patch_mx_y_range = range(patch_mx_size[1])
+        if self._use_zero_padding:
+            input = np.pad(input, ((0, 0), (x_patch_radius, x_patch_radius), (y_patch_radius, y_patch_radius)))
 
-                for patch_mx_y in patch_mx_y_range:
-                    input_y = patch_mx_y + y_offset + y_diff
+        x_diff_range = range(x_patch_radius * 2 + 1)
+        y_diff_range = range(y_patch_radius * 2 + 1)
+        
+        patch_mx_size = (self._num_channels * self._patch_size[0] * self._patch_size[1], 
+                        input.shape[1] - x_patch_radius * 2, 
+                        input.shape[2] - y_patch_radius * 2)
+        patch_mx = np.empty(patch_mx_size)
 
-                    patch_mx_pixel_row = patch_mx_y * patch_mx_size[0]
-                    input_pixel_row = input_y * self._input_size[0]
+        for patch_mx_x in range(patch_mx_size[1]):
+            for patch_mx_y in range(patch_mx_size[2]):
+                channel_offset = 0
 
-                    for patch_mx_x in patch_mx_x_range:
-                        input_x = patch_mx_x + x_offset + x_diff 
+                for x_diff in x_diff_range:
+                    for y_diff in y_diff_range:
+                        input_x = patch_mx_x + x_diff
+                        input_y = patch_mx_y + y_diff
 
-                        patch_mx_pixel = patch_mx_x + patch_mx_pixel_row
-                        input_pixel = input_x + input_pixel_row
+                        for channel in range(self._num_channels):
+                            patch_mx[channel_offset + channel][patch_mx_x][patch_mx_y] = input[channel][input_x][input_y]
                         
-                        for channel in range(channels):
-                            patch_mx[current_patch_mx_row + channel][patch_mx_pixel] = input[channel][input_pixel]
+                        channel_offset += self._num_channels
 
-                current_patch_mx_row += channels
-
-        return patch_mx                            
+        patch_mx = np.reshape(patch_mx, (patch_mx_size[0], patch_mx_size[1] * patch_mx_size[2]))
+        return patch_mx                  
 
 
     def forward(self, input):

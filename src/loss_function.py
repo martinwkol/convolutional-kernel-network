c9ebc23dd379c5994e99c532197283096c9c4659
Martin Koloseus <mkoloseus@posteo.de>
2022-11-20 19:50:58 +0100

    feat: loss_function class & mean squared error implemented

---
+++ src/loss_function.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class loss_function:
+    def __init__(self, loss, gradient):
+        self._loss = loss
+        self._gradient = gradient
+
+    def loss(self, predicted, expected):
+        return self._loss(predicted, expected)
+
+    def gradient(self, predicted, expected):
+        return self._gradient(predicted, expected)
+
+mse = loss_function(
+    loss=lambda predicted, expected: 1 / predicted.shape[0] * np.square(predicted - expected).sum(), 
+    gradient=lambda predicted, expected: 2 * (predicted - expected) / predicted.shape[0]
+)

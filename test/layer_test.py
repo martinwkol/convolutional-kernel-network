1c776393252ba8dfdf583cd9f6eccdd827b0b45f
Martin Koloseus <mkoloseus@posteo.de>
2022-11-22 20:07:55 +0100

    test(layer): test avg_pooling

--- test/layer_test.py
+++ test/layer_test.py
@@ -11,15 +11,16 @@ from src.kernel import get_rbf
 from src.layer import layer
 
 class LayerTest(unittest.TestCase):
+    @staticmethod
+    def random_filter_matrix(shape):
+        filter_mx = np.random.rand(shape[0], shape[1])
+        norms = np.linalg.norm(filter_mx, axis = 0)
+        return filter_mx * (1 / norms)
+
     def setUp(self):
-        self.filter_mx_3x3x1 = np.array([
-            [1,     0, 0,   0, 0,   0, 0,   0, 0], 
-            [0.25,  0, 0.25,0, 0.25,0, 0.25,0, 0]
-        ]).transpose()
-        self.filter_mx_3x3x2 = 1/2 * np.array([
-            [1,     0, 0,   0, 0,   0, 0,   0, 0, 1,     0, 0,   0, 0,   0, 0,   0, 0], 
-            [0.25,  0, 0.25,0, 0.25,0, 0.25,0, 0, 0.25,  0, 0.25,0, 0.25,0, 0.25,0, 0]
-        ]).transpose()
+        self.filter_mx_1x1x1 = LayerTest.random_filter_matrix((1*1*1, 2))
+        self.filter_mx_3x3x1 = LayerTest.random_filter_matrix((3*3*1, 2))
+        self.filter_mx_3x3x2 = LayerTest.random_filter_matrix((3*3*2, 2))
 
     def test_extract_patches_numpy_without_zero_padding(self):
         l = layer(
@@ -119,6 +120,36 @@ class LayerTest(unittest.TestCase):
         ]).transpose()
         
         self.assertTrue((adj == expectedAdj).all())
+
+
+
+    def test_avg_pooling(self):
+        l = layer(
+            input_size=(5, 5), num_channels=1, filter_size=(1, 1), 
+            pooling_size=(2, 2), dp_kernel=get_rbf(1), 
+            filter_matrix=self.filter_mx_1x1x1,
+            zero_padding=(0, 0)
+        )
+
+        U = np.array([[
+            1., 2., 3., 4., 5.,
+            2., 3., 4., 5., 6.,
+            3., 4., 5., 6., 7.,
+            4., 5., 6., 7., 8.,
+            5., 6., 7., 8., 9.
+        ]])
+
+        pooled = l.avg_pooling(U)
+
+        expected_pooled = np.array([[
+            2., 4.,
+            4., 6.
+        ]])
+
+        self.assertTrue((pooled == expected_pooled).all())
+
+
+
 
     def test_forward_doesnt_crash(self):
         l = layer(

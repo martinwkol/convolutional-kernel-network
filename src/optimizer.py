cc7aef54ceab669cf13b016d079285ed318de0ad
Martin Koloseus <mkoloseus@posteo.de>
2023-01-15 22:50:14 +0100

    feat: move responsibilities from optimizer to trainer

--- src/optimizer.py
+++ src/optimizer.py
@@ -2,29 +2,28 @@ from copy import deepcopy
 import numpy as np
 
 class Optimizer:
-    def __init__(self, network, learning_rate, loss_function, output_activation_func = None, max_loss_increase = 2):
-        self.network = network
-        self.learning_rate = learning_rate
+    def __init__(self, loss_function, network = None, output_activation_func = None, max_loss_increase = 2):
         self.loss_function = loss_function
+        self.network = network
         # TODO: find better name
         self.output_activation_func = output_activation_func if output_activation_func is not None else lambda x: x
-        self.max_loss_increase = max_loss_increase
-
-        self.best_network = network
-        self.loss_history = []
-        self.loss_increase_counter = 0
-        self.min_loss = float('inf')
 
         self.loss_sum = 0
         self.gradent_sum = None
-        self.current_batch_size = 0
+
+    
+    def set_network(self, network):
+        self.network = network
+        self.loss_sum = 0
+        self.gradent_sum = None
 
 
     def step(self, training_input, expected_output):
+        # TODO: network is NULL except
+
         pred_raw = self.network.forward(training_input)
         pred = self.output_activation_func(pred_raw)
 
-        self.current_batch_size += 1
         self.loss_sum += self.loss_function.loss(predicted=pred, expected=expected_output)
 
         gradients = self.network.gradients(loss_func=self.loss_function, expected_output=expected_output)
@@ -39,50 +38,26 @@ class Optimizer:
                 continue
             self.gradent_sum[0][j] += gradients[0][j]
 
-        #print(self.gradent_sum[1].shape)
-        #print(gradients[1].shape)
+
         self.gradent_sum[1] += gradients[1]
 
 
-
-    def optim(self):
-        if self.current_batch_size == 0:
-            return
-
-        adjusted_learning_rate = self.learning_rate * self.current_batch_size
-        adjusted_loss = self.loss_sum / self.current_batch_size
+    def optim(self, learning_rate):
+        # TODO: network is NULL except
 
         for j in range(len(self.gradent_sum[0])):
             if self.gradent_sum[0][j] is None:
                 continue
 
             new_filter_matrix = self.network.layers[j].filter_matrix - \
-                                adjusted_learning_rate * self.gradent_sum[0][j] 
+                                learning_rate * self.gradent_sum[0][j] 
             norms = np.linalg.norm(new_filter_matrix, axis = 0)
             self.network._layers[j].update_filter_matrix(new_filter_matrix / norms)
         
-        self.network.output_weights -= adjusted_learning_rate * self.gradent_sum[1]
+        self.network.output_weights -= learning_rate * self.gradent_sum[1]
 
-        if adjusted_loss < self.min_loss:
-            self.min_loss = adjusted_loss
-            self.loss_increase_counter = 0
-            # TODO: more efficient copy
-            self.best_network = deepcopy(self.network)
-
-        elif adjusted_loss > self.min_loss:
-            self.loss_increase_counter += 1
-
-            if self.loss_increase_counter >= self.max_loss_increase:
-                self.learning_rate /= 2
-                # TODO: more efficient copy
-                self.network = deepcopy(self.best_network)
-                self.loss_increase_counter = 0
-
-
-        self.loss_history.append(adjusted_loss)
-        
+        loss = self.loss_sum 
         self.loss_sum = 0
         self.gradent_sum = None
-        self.current_batch_size = 0
 
-        return adjusted_loss
+        return loss
--- src/trainer.py
+++ src/trainer.py
@@ -1,8 +1,10 @@
 import numpy as np
 
 class Trainer:
-    def __init__(self, optimizer, batch_size, train_input, train_output):
+    def __init__(self, network, optimizer, learning_rate, batch_size, train_input, train_output):
+        self.network = network
         self.optimizer = optimizer
+        self.learning_rate = learning_rate
         self.batch_size = batch_size
         self.train_input = train_input
         self.train_output = train_output
@@ -10,6 +12,8 @@ class Trainer:
         self.batch_counter = 0
 
     def train(self, epochs):
+        self.optimizer.set_network(network=self.network)
+
         for _ in range(epochs):
             permutation = np.random.permutation(len(self.train_input))
 
@@ -18,5 +22,5 @@ class Trainer:
                 self.batch_counter += 1
 
                 if self.batch_counter >= self.batch_size:
-                    self.optimizer.optim()
+                    self.optimizer.optim(self.learning_rate)
                     self.batch_counter = 0

b9b11778241c06ab96dd3dc504d33910eb416372
Martin Koloseus <mkoloseus@posteo.de>
2023-01-16 10:45:26 +0100

    feat(trainer): reduce learning rate if loss increases between 2 epochs

--- src/trainer.py
+++ src/trainer.py
@@ -1,4 +1,5 @@
 import numpy as np
+from copy import deepcopy
 
 class Trainer:
     def __init__(self, network, optimizer, learning_rate, batch_size, train_input, train_output):
@@ -10,17 +11,32 @@ class Trainer:
         self.train_output = train_output
 
         self.batch_counter = 0
+        self.last_average_loss = float('inf')
+        self.best_network = network
 
     def train(self, epochs):
         self.optimizer.set_network(network=self.network)
 
         for _ in range(epochs):
             permutation = np.random.permutation(len(self.train_input))
+            loss_sum = 0
+            optimized_data_counter = 0
 
             for index in permutation:
                 self.optimizer.step(self.train_input[index], self.train_output[index])
                 self.batch_counter += 1
 
                 if self.batch_counter >= self.batch_size:
-                    self.optimizer.optim(self.learning_rate)
+                    loss_sum += self.optimizer.optim(self.learning_rate)
+                    optimized_data_counter += self.batch_counter
                     self.batch_counter = 0
+
+            average_loss = loss_sum / optimized_data_counter
+            if average_loss < self.last_average_loss:
+                self.last_average_loss = average_loss
+                self.best_network = deepcopy(self.network)
+
+            elif average_loss > self.last_average_loss:
+                self.network = deepcopy(self.best_network)
+                self.learning_rate /= 2
+

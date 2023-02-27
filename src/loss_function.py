e00c024defcde7c8997eb3e40151f0296eacfb78
Martin Koloseus <mkoloseus@posteo.de>
2023-02-27 15:00:41 +0100

    refactor(loss_function): seperate classes for each type of loss function

--- src/loss_function.py
+++ src/loss_function.py
@@ -1,38 +1,37 @@
 import numpy as np
 
 class LossFunction:
-    def __init__(self, loss, gradient):
-        self._loss = loss
-        self._gradient = gradient
-
     def loss(self, predicted, expected):
-        return self._loss(predicted, expected)
+        raise NotImplementedError()
 
     def gradient(self, predicted, expected):
-        return self._gradient(predicted, expected)
+        raise NotImplementedError()
 
-def get_square_hinge_loss(margin):
-    def loss(predicted, expected):
-        loss_array_with_expected = np.maximum(margin + predicted - predicted[expected], 0)
+class SquareHingeLoss(LossFunction):
+    def __init__(self, margin):
+        self.margin = margin
+    
+    def loss(self, predicted, expected):
+        loss_array_with_expected = np.maximum(self.margin + predicted - predicted[expected], 0)
         loss_array = loss_array_with_expected[np.arange(len(predicted)) != expected]
         return loss_array.sum() ** 2 / len(loss_array)
-
-    def gradient(predicted, expected):
-        loss_array_with_expected = np.maximum(margin + predicted - predicted[expected], 0)
+    
+    def gradient(self, predicted, expected):
+        loss_array_with_expected = np.maximum(self.margin + predicted - predicted[expected], 0)
         loss_array = loss_array_with_expected[np.arange(len(predicted)) != expected]
 
         grad = (loss_array_with_expected > 0).astype(float)
         grad[expected] = -np.count_nonzero(loss_array)
-        grad *= 2 * loss_array.sum()  
-
+        grad *= 2 * loss_array.sum()
+        
         return grad / len(loss_array)
 
-    return LossFunction(
-        loss=loss, 
-        gradient=gradient
-    )
+class MeanSquaredError(LossFunction):
+    def __init__(self):
+        pass
 
-mse = LossFunction(
-    loss=lambda predicted, expected: 1 / predicted.shape[0] * np.square(predicted - expected).sum(), 
-    gradient=lambda predicted, expected: 2 * (predicted - expected) / predicted.shape[0]
-)
+    def loss(self, predicted, expected):
+        return 1 / predicted.shape[0] * np.square(predicted - expected).sum()
+    
+    def gradient(self, predicted, expected):
+        return 2 * (predicted - expected) / predicted.shape[0]

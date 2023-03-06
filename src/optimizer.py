a855813c44ce7d71e44b31c59c2c3b397dd06764
Martin Koloseus <mkoloseus@posteo.de>
2023-03-06 22:18:41 +0100

    fix(optimizer): apply regularization to output weights first

--- src/optimizer.py
+++ src/optimizer.py
@@ -22,8 +22,8 @@ class Optimizer:
     def step(self, training_input, expected_output):
         # TODO: network is NULL except
 
-        pred = self._network.forward(training_input)
-        self._loss_sum += self.loss_function.loss(predicted=pred, expected=expected_output)
+        predicted = self._network.forward(training_input)
+        self._loss_sum += self.loss_function.loss(predicted=predicted, expected=expected_output)
         loss_func_gradient = self.loss_function.gradient(self._network.last_output, expected_output)
         gradients = self._network.gradients(loss_func_gradient)
         
@@ -42,14 +42,14 @@ class Optimizer:
         if self._num_steps == 0:
             return
 
-        grad_sum_scalar = learning_rate / self._num_steps
+        gradient_sum_multiplier = learning_rate / self._num_steps
         regularization_term = np.sum(self.network.output_weights * self.network.output_weights) * regularization_parameter / 2
 
         for j in range(len(self._gradent_sum) - 1):
-            self.network.layers[j].gradient_descent(grad_sum_scalar * self._gradent_sum[j])
+            self.network.layers[j].gradient_descent(gradient_sum_multiplier * self._gradent_sum[j])
         
-        self.network.output_weights -= grad_sum_scalar * self._gradent_sum[-1] 
         self.network.output_weights *= 1 - learning_rate * regularization_parameter
+        self.network.output_weights -= gradient_sum_multiplier * self._gradent_sum[-1] 
 
         loss = self._loss_sum / self._num_steps + regularization_term
         self.reset()

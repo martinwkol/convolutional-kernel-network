594dd81a523651b0ecf5376590b441e9758d4b93
Martin Koloseus <mkoloseus@posteo.de>
2023-03-07 21:44:33 +0100

    feat(trainer): trainer no longer saves training data when pickled

--- src/trainer.py
+++ src/trainer.py
@@ -1,22 +1,20 @@
 import numpy as np
 from copy import deepcopy
+import pickle
 
 class Trainer:
-    def __init__(self, optimizer, learning_rate, regularization_parameter, batch_size, train_input, train_output):
+    def __init__(self, optimizer, learning_rate, regularization_parameter, batch_size, train_input = None, train_output = None):
         self.optimizer = optimizer
         self.learning_rate = learning_rate
         self.regularization_parameter = regularization_parameter
         self.batch_size = batch_size
-        self.train_input = train_input
-        self.train_output = train_output
+        self.set_training_data(train_input, train_output)
 
         self.best_network = deepcopy(self.optimizer.network)
 
         self._best_average_loss_epoch = float('inf')
         self._average_loss_batch = []
         self._average_loss_epoch = []
-        
-        self._new_epoch()
 
     
     @property
@@ -48,9 +46,29 @@ class Trainer:
         return self._permutation_index
 
 
+    def __getstate__(self):
+        return { k: v for (k, v) in self.__dict__.items() if k not in ["_train_input", "_train_output"] }
+
+    def __setstate__(self, d):
+        self.__dict__ = d
+        self._train_input = None
+        self._train_output = None
+
+
+    def set_training_data(self, train_input, train_output):
+        assert len(train_input) == len(train_output)
+
+        self._train_input = train_input
+        self._train_output = train_output
+        self._new_epoch()       
+
+
     def next_image(self):
+        if self._train_input is None or self._train_output is None:
+            raise TypeError("train_input or train_output is None")
+
         index = self._permutation[self._permutation_index]
-        self.optimizer.step(self.train_input[index], self.train_output[index])
+        self.optimizer.step(self._train_input[index], self._train_output[index])
         self._batch_counter += 1
         self._permutation_index += 1
 
@@ -92,7 +110,7 @@ class Trainer:
 
 
     def _new_epoch(self):
-        self._permutation = np.random.permutation(len(self.train_input))
+        self._permutation = np.random.permutation(len(self._train_input))
         self._permutation = self._permutation[:len(self._permutation) - (len(self._permutation) % self.batch_size)]
         self._permutation_index = 0
         self._batch_counter = 0

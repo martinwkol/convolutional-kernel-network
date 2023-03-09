bd643e05f008f12447080e6e3810b4975fb7062c
Martin Koloseus <mkoloseus@posteo.de>
2023-03-09 18:57:04 +0100

    feat&fix(experiments): save and load implemented & false spelling corrected

--- src/experiments.py
+++ src/experiments.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import pickle
 
 class TestResult:
     def __init__(self, network_pred):
@@ -15,7 +16,7 @@ class TestResult:
         self.label_correct_count = network_pred.diagonal()
         self.label_false_count = self.label_count - self.label_correct_count
         self.label_correct_portion = self.label_correct_count / self.label_count
-        self.label_false_count = self.label_false_count / self.label_count
+        self.label_false_portion = self.label_false_count / self.label_count
         
 
 class Experiment:
@@ -26,6 +27,18 @@ class Experiment:
         self.num_labels = num_labels
         self.test_results_epoch = []
         self.test_results_batch = []
+
+    @staticmethod
+    def load(filepath):
+        f = open(filepath, "rb")
+        experiment = pickle.load(f)
+        f.close()
+        return experiment
+
+    def save(self, filepath):
+        f = open(filepath, "wb")
+        pickle.dump(self, f)
+        f.close()
 
     def perform_experiment(self, epochs, batches_per_test=math.inf, num_test=math.inf):
         batches_per_test = min(batches_per_test, self.trainer.epoch_size)
@@ -39,7 +52,7 @@ class Experiment:
                     self.test_results_batch.append(test_result)
                     batch_counter = 0
 
-                while trainer.epoch_counter == 0:
+                if self.trainer.epoch_counter == 0:
                     test_result = self.perform_test()
                     self.test_results_epoch.append(test_result)
                     break
@@ -50,7 +63,7 @@ class Experiment:
         for j in range(min(num_tests, len(self.test_images))):
             pred_enc = self.trainer.best_network.forward(self.test_images[j])
             pred = np.argmax(pred_enc)
-            network_pred[self.test_labels[i]][pred] += 1
+            network_pred[self.test_labels[j]][pred] += 1
 
         return TestResult(network_pred)
 

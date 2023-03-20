21e3ea63e0c635740a8ad16240e7c2e3ef1d5a39
Martin Koloseus <mkoloseus@posteo.de>
2023-03-20 23:53:04 +0100

    refactor(main_create_analyses): import create_mnist_trainer from main_train.py

--- src/main_create_analyses.py
+++ src/main_create_analyses.py
@@ -15,59 +15,7 @@ import loss_function
 import optimizer as op
 import trainer as tr
 from analysis import Analysis
-
-
-def create_mnist_trainer(data, model_layers):
-    net = network.Network(input_size=(28, 28), in_channels=1, layer_infos=model_layers, output_nodes=10)
-    optimizer = op.Optimizer(network=net, loss_function=loss_function.SquareHingeLoss(margin=0.2))
-    trainer = tr.Trainer(
-        optimizer=optimizer, batch_size=128, learning_rate=2, regularization_parameter=1/60000,
-        train_images=data.train_images, train_labels=data.train_labels
-    )
-    return trainer
-
-
-def perform_test(trainer, test_images, test_labels, num_tests=math.inf):
-    correct_preds = 0
-
-    for j in range(min(num_tests, len(test_images))):
-        pred_enc = trainer.optimizer.network.forward(test_images[j])
-        pred = np.argmax(pred_enc)
-        if pred == test_labels[j]:
-            correct_preds += 1
-
-    return correct_preds
-
-
-def train_network(trainer, epochs, batches_per_test, test_images, test_labels, num_tests_batch=math.inf, num_tests_epoch=math.inf):
-    batch_counter = 0
-    num_tests_batch = min(num_tests_batch, len(test_images))
-    num_tests_epoch = min(num_tests_epoch, len(test_images))
-
-    for _ in range(epochs):
-        print(f"Epoch: {trainer.epoch}")
-        while True:
-            print(f"[E{trainer.epoch}, {trainer.epoch_counter}]", end='\r')
-            
-            trainer.finish_batch()
-            batch_counter += 1
-
-            if batch_counter >= batches_per_test:
-                print(f"Test of size {num_tests_batch}\t\t")
-                correct_preds = perform_test(trainer, test_images, test_labels, num_tests_batch)
-                accuracy = 100.0 * correct_preds / num_tests_batch
-                print(f"Accuracy: {correct_preds}/{num_tests_batch} ({accuracy:.2f}%)\n")
-                batch_counter = 0
-
-            if trainer.epoch_counter == 0:
-                print(f"Test of size {num_tests_epoch}\t\t")
-                correct_preds = perform_test(trainer, test_images, test_labels, num_tests_epoch)
-                accuracy = 100.0 * correct_preds / num_tests_epoch
-                print(f"Accuracy: {correct_preds}/{num_tests_epoch} ({accuracy:.2f}%)\n")
-                print('-' * 20)
-                print()
-                break
-
+from main_train import create_mnist_trainer
 
 
 def create_analysis(mnist, filepath, epochs, trainer, batches_per_test=100, num_tests_batch=1000, num_tests_epoch=math.inf):
@@ -78,7 +26,6 @@ def create_analysis(mnist, filepath, epochs, trainer, batches_per_test=100, num_
 
     while analysis.trainer.epoch <= epochs:
         print("Epoch {}".format(analysis.trainer.epoch))
-        print("Learning rate {}".format(analysis.trainer.learning_rate))
         analysis.perform_analysis(epochs=1, batches_per_test=batches_per_test, num_tests_batch=num_tests_batch)
         analysis.save_to_file(filepath)
         print(str(analysis.test_results_epoch[-1]))

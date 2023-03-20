22be26b188c783852ef4173f96d1b5916d79f322
Martin Koloseus <mkoloseus@posteo.de>
2023-03-20 02:12:24 +0100

    refactor(tests): adapt tests to changes

--- test/layer_test.py
+++ test/layer_test.py
@@ -6,9 +6,11 @@ import os
 current_directory = os.path.dirname(os.path.realpath(__file__))
 parent_directory = os.path.dirname(current_directory)
 sys.path.append(parent_directory)
+sys.path.append("src/")
 
-from src.kernel import get_rbf
-from src.internal_filter_layer import IntFilterLayer
+
+from src.kernel import RadialBasisFunction
+from src.filter_layer import FilterLayer
 
 class LayerTest(unittest.TestCase):
     @staticmethod
@@ -23,9 +25,9 @@ class LayerTest(unittest.TestCase):
         self.filter_mx_3x3x2 = LayerTest.random_filter_matrix((3*3*2, 2))
 
     def test_extract_patches_without_zero_padding(self):
-        l = IntFilterLayer(
+        l = FilterLayer(
             input_size=(3, 3), in_channels=2, filter_size=(3, 3), 
-            dp_kernel=get_rbf(1), filter_matrix=self.filter_mx_3x3x2,
+            dp_kernel=RadialBasisFunction(1), filter_matrix=self.filter_mx_3x3x2,
             zero_padding=(0, 0)
         )
         input = np.array([
@@ -33,7 +35,7 @@ class LayerTest(unittest.TestCase):
             [21, 22], [23, 24], [25, 26], 
             [31, 32], [33, 34], [35, 36]
         ]).transpose()
-        patched = l.extract_patches(input)
+        patched = l._extract_patches(input)
 
         self.assertEqual(patched.shape, (2 * l.filter_size[0] * l.filter_size[1], 1))
         expectedPatched = np.array([
@@ -46,9 +48,9 @@ class LayerTest(unittest.TestCase):
         self.assertTrue((patched == expectedPatched).all())
 
     def test_extract_patches_zero_padding(self):
-        l = IntFilterLayer(
+        l = FilterLayer(
             input_size=(3, 3), in_channels=2, filter_size=(3, 3), 
-            dp_kernel=get_rbf(1), filter_matrix=self.filter_mx_3x3x2,
+            dp_kernel=RadialBasisFunction(1), filter_matrix=self.filter_mx_3x3x2,
             zero_padding=(1, 1)
         )
         input = np.array([
@@ -56,7 +58,7 @@ class LayerTest(unittest.TestCase):
             [21, 22], [23, 24], [25, 26], 
             [31, 32], [33, 34], [35, 36]
         ]).transpose()
-        patched = l.extract_patches(input)
+        patched = l._extract_patches(input)
 
         self.assertEqual(patched.shape, (2 * l.filter_size[0] * l.filter_size[1], l.input_size[0] * l.input_size[1]))
         expectedPatched = np.array([
@@ -74,19 +76,19 @@ class LayerTest(unittest.TestCase):
         self.assertTrue((patched == expectedPatched).all())
 
     def test_extract_patches_adj_without_zero_padding(self):
-        l = IntFilterLayer(
+        l = FilterLayer(
             input_size=(3, 3), in_channels=2, filter_size=(3, 3), 
-            dp_kernel=get_rbf(1), filter_matrix=self.filter_mx_3x3x2,
+            dp_kernel=RadialBasisFunction(1), filter_matrix=self.filter_mx_3x3x2,
             zero_padding=(0, 0)
         )
 
-        patched = l.extract_patches(np.array([
+        patched = l._extract_patches(np.array([
             [1, 0],     [1, 3],     [1, 0], 
             [1, 3],     [1, 0],     [1, 3], 
             [1, 0],     [1, 3],     [1, 0]
         ]).transpose())
 
-        adj = l.extract_patches_adj(patched)
+        adj = l._extract_patches_adj(patched)
         self.assertEqual(adj.shape, (2, 9))
 
         expectedAdj = np.array([
@@ -95,22 +97,24 @@ class LayerTest(unittest.TestCase):
             [1, 0],     [1, 3],     [1, 0]
         ]).transpose()
 
+        print(adj.transpose())
+
         self.assertTrue((adj == expectedAdj).all())
 
     def test_extract_patches_adj_with_zero_padding(self):
-        l = IntFilterLayer(
+        l = FilterLayer(
             input_size=(3, 3), in_channels=2, filter_size=(3, 3), 
-            dp_kernel=get_rbf(1), filter_matrix=self.filter_mx_3x3x2,
+            dp_kernel=RadialBasisFunction(1), filter_matrix=self.filter_mx_3x3x2,
             zero_padding=(1, 1)
         )
 
-        patched = l.extract_patches(np.array([
+        patched = l._extract_patches(np.array([
             [1, 3],     [1, 2],     [1, 1], 
             [1, 2],     [1, 1],     [1, 0], 
             [1, 1],     [1, 0],     [1, 0]
         ]).transpose())
 
-        adj = l.extract_patches_adj(patched)
+        adj = l._extract_patches_adj(patched)
         self.assertEqual(adj.shape, (2, 9))
 
         expectedAdj = np.array([
@@ -118,24 +122,27 @@ class LayerTest(unittest.TestCase):
             [6, 12],    [9, 9],     [6, 0], 
             [4, 4],     [6, 0],     [4, 0]
         ]).transpose()
+
+        #print(adj.transpose())
+
         
         self.assertTrue((adj == expectedAdj).all())
 
 
 
     def test_forward_doesnt_crash(self):
-        l = IntFilterLayer(
+        l = FilterLayer(
             input_size=(3, 3), in_channels=1, filter_size=(3, 3), 
-             dp_kernel=get_rbf(1), 
+             dp_kernel=RadialBasisFunction(1), 
             filter_matrix=self.filter_mx_3x3x1,
             zero_padding=(0, 0)
         )
         l.forward(np.array([[1, 1, 1, 1, 0, 1, 1, 1, 1]]))
 
     def test_g_doesnt_crash(self):
-        l = IntFilterLayer(
+        l = FilterLayer(
             input_size=(3, 3), in_channels=1, filter_size=(3, 3), 
-            dp_kernel=get_rbf(1), 
+            dp_kernel=RadialBasisFunction(1), 
             filter_matrix=self.filter_mx_3x3x1,
             zero_padding=(1, 1)
         )
@@ -145,14 +152,14 @@ class LayerTest(unittest.TestCase):
             [0.2, 0.1], [0.1, 0.3], [0.6, 0.4],
             [0.5, 0.9], [0.2, 0.1], [0.1, 0.8],
         ]).transpose()
-        B = l.calculate_B(U)
-        C = l.calculate_C(U, output)
-        l.g(B, C)
+        B = l._calculate_B(U)
+        C = l._calculate_C(U, output)
+        l._g(B, C)
 
     def test_h_doesnt_crash(self):
-        l = IntFilterLayer(
+        l = FilterLayer(
             input_size=(3, 3), in_channels=1, filter_size=(3, 3), 
-            dp_kernel=get_rbf(1), 
+            dp_kernel=RadialBasisFunction(1), 
             filter_matrix=self.filter_mx_3x3x1,
             zero_padding=(1, 1)
         )
@@ -162,8 +169,8 @@ class LayerTest(unittest.TestCase):
             [0.2, 0.1], [0.1, 0.3], [0.6, 0.4],
             [0.5, 0.9], [0.2, 0.1], [0.1, 0.8],
         ]).transpose()
-        B = l.calculate_B(U)
-        l.h(U, B)
+        B = l._calculate_B(U)
+        l._h(U, B)
 
 
 if __name__ == '__main__':

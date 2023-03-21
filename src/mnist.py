a433401f61aaabf4b45d3f78980e0cab9f8a0496
Martin Koloseus <mkoloseus@posteo.de>
2023-03-21 14:52:04 +0100

    fix&feat(mnist): add missing function call of download_mnist & print "Downloading {url} => {file_path}"

--- src/mnist.py
+++ src/mnist.py
@@ -9,6 +9,8 @@ import numpy as np
 
 class MNIST:
     def __init__(self, directory):
+        MNIST.download_mnist(directory)
+
         with gzip.open(os.path.join(directory, 'train-images-idx3-ubyte.gz'), 'rb') as f:
             self.train_images = np.frombuffer(f.read(), np.uint8, offset=16).reshape(-1, 28*28) / 255
         with gzip.open(os.path.join(directory, 'train-labels-idx1-ubyte.gz'), 'rb') as f:
@@ -31,4 +33,5 @@ class MNIST:
 
             if not os.path.exists(file_path):
                 url = base_url + file_name
+                print(f"Downloading {url} => {file_path}")
                 urllib.request.urlretrieve(url, file_path)
